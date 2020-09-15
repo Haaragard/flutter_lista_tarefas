41b6952dc4557063970d0f24198961b6deb0fd55
Haaragard <diogololz159@gmail.com>
2020-09-15 04:47:57 -0300

    Added creation and deleting of ToDos, added temporary recuperation by snackbar of deleted ToDos and more

--- lib/main.dart
+++ lib/main.dart
@@ -26,11 +26,18 @@ class _HomeState extends State<Home> {
   bool _addEnabled = false;
 
   List _toDoList = [];
+  Map<String, dynamic> _toDoRemoved;
+  int _toDoRemovedPos;
 
   void initState() {
     super.initState();
     _readData().then((data) {
-      _toDoList = (data != null) ? json.decode(data) : [];
+      try {
+        _toDoList = (data != null) ? json.decode(data) : [];
+      } catch (e) {
+        _toDoList = [];
+      }
+
       _addEnabled = true;
     });
   }
@@ -56,6 +63,45 @@ class _HomeState extends State<Home> {
     });
 
     _saveData();
+  }
+
+  void _removeDismissible(int index) {
+    setState(() {
+      _toDoRemoved = Map.from(_toDoList[index]);
+      _toDoRemovedPos = index;
+      _toDoList.removeAt(index);
+    });
+
+    _saveData();
+  }
+
+  void _recoverDismissible(int index, BuildContext context) {
+    final snackBar = SnackBar(
+      content: Text("Tarefa \"${_toDoRemoved["title"]}\" removida!"),
+      action: SnackBarAction(label: "Desfazer",
+        onPressed: () {
+          setState(() {
+            _toDoList.insert(_toDoRemovedPos, _toDoRemoved);
+            _saveData();
+          });
+
+        },
+      ),
+      duration: Duration(seconds: 2,),
+    );
+    Scaffold.of(context).removeCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  Future<Null> _refresh() async  {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["done"] && !b["done"]) return 1;
+        else if (!a["done"] && b["done"]) return -1;
+        else return 0;
+      });
+    });
   }
 
   @override
@@ -96,31 +142,56 @@ class _HomeState extends State<Home> {
         ),
 
         Expanded(
-          child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: (context, index) {
-                return CheckboxListTile(
-                  title: Text("${_toDoList[index]["title"]}"),
-                  value: _toDoList[index]["done"],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                        (_toDoList[index]["done"]) ? Icons.check : Icons.error
-                    ),
-                  ),
-                  onChanged: (status) {
-                    _changeStatusToDo(index, status);
-                  },
-                );
-              }
+          child: RefreshIndicator(
+            onRefresh: _refresh,
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: _buildItem
+            ),
           ),
         )
       ],
     );
   }
 
+  Widget _buildItem(BuildContext context, int index) {
+    return Dismissible(
+      key: UniqueKey(),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Padding(padding: EdgeInsets.only(left: 15.0),
+            child: Icon(Icons.delete, color: Colors.white,),
+          ),
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text("${_toDoList[index]["title"]}"),
+        value: _toDoList[index]["done"],
+        secondary: CircleAvatar(
+          child: Icon(
+              (_toDoList[index]["done"]) ? Icons.check : Icons.error
+          ),
+        ),
+        onChanged: (status) { _changeStatusToDo(index, status); },
+      ),
+      onDismissed: (direction) {
+        _removeDismissible(index);
+        _recoverDismissible(index, context);
+      },
+    );
+  }
+
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
+
+    final File file = File("${directory.path}/data.json");
+    if (!(await file.exists())) {
+      await file.create();
+    }
     return File("${directory.path}/data.json");
   }
 
